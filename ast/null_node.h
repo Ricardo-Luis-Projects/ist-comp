601414b344f21f812437eaff56c1f705bc765c27
Ricardo Antunes <me@riscadoa.com>
2023-05-14 11:08:41 +0100

    refactor: make null_node a literal

--- ast/null_node.h
+++ ast/null_node.h
@@ -1,17 +1,17 @@
 #ifndef __MML_AST_NULLPTR_H__
 #define __MML_AST_NULLPTR_H__
 
-#include <cdk/ast/expression_node.h>
+#include <cdk/ast/literal_node.h>
 
 namespace mml {
 
   /**
    * Class for describing null nodes. 
    */
-  class null_node: public cdk::expression_node {
+  class null_node: public virtual cdk::literal_node<std::nullptr_t> {
   public:
     inline null_node(int lineno) :
-        cdk::expression_node(lineno) {
+        cdk::literal_node<std::nullptr_t>(lineno, nullptr) {
     }
 
   public:
