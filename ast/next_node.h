e6c2257afb4219aceaaca5250e52770779c44885
Luís Fonseca <luis@lhf.pt>
2023-05-14 17:06:32 +0100

    refactor: remove loop_flow_control_node

--- ast/next_node.h
+++ ast/next_node.h
@@ -1,21 +1,26 @@
 #ifndef __MML_AST_NEXT_NODE_H__
 #define __MML_AST_NEXT_NODE_H__
 
-#include <ast/loop_flow_control_node.h>
+#include <cdk/ast/basic_node.h>
 
 namespace mml {
 
   /**
    * Class for describing next nodes.
    */
-  class next_node : public mml::loop_flow_control_node {
+  class next_node : public cdk::basic_node {
+    int _nesting;
 
   public:
     inline next_node(int lineno, int nesting) :
-        mml::loop_flow_control_node(lineno, nesting) {
+       cdk::basic_node(lineno), _nesting(nesting) {
     }
 
   public:
+    inline int nesting() {
+      return _nesting;
+    }
+    
     void accept(basic_ast_visitor *sp, int level) {
       sp->do_next_node(this, level);
     }
--- ast/stop_node.h
+++ ast/stop_node.h
@@ -1,21 +1,26 @@
 #ifndef __MML_AST_STOP_NODE_H__
 #define __MML_AST_STOP_NODE_H__
 
-#include <ast/loop_flow_control_node.h>
+#include <cdk/ast/basic_node.h>
 
 namespace mml {
 
   /**
    * Class for describing stop nodes.
    */
-  class stop_node : public mml::loop_flow_control_node {
+  class stop_node : public cdk::basic_node {
+    int _nesting;
 
   public:
     inline stop_node(int lineno, int nesting) :
-        mml::loop_flow_control_node(lineno, nesting) {
+        cdk::basic_node(lineno), _nesting(nesting) {
     }
 
   public:
+    inline int nesting() {
+      return _nesting;
+    }
+    
     void accept(basic_ast_visitor *sp, int level) {
       sp->do_stop_node(this, level);
     }
